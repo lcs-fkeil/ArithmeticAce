4a82e0ae4e7f9441623f75d35feb4bf1e9627787
Berk Bozkurt <BBozkurt@lcs.on.ca>
2022-02-07 08:52:16 -0500

    Initial Commit

---
+++ Arithmetic Ace/Arithmetic_AceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arithmetic_AceApp.swift
+//  Arithmetic Ace
+//
+//  Created by Berk Bozkurt on 2022-02-07.
+//
+
+import SwiftUI
+
+@main
+struct Arithmetic_AceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
