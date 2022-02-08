231a06fca9cdc228a310cbeef7a27a7e45782308
Franka Keil <fkeil@lcs.on.ca>
2022-02-07 21:03:37 -0500

    Added a addition view

--- Arithmetic Ace/AdditionView.swift
+++ Arithmetic Ace/AdditionView.swift
@@ -8,11 +8,84 @@
 import SwiftUI
 
 struct AdditionView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    // MARK: Stored properties
+        let additionand = Int.random(in: 1...12)
+        let additioner = Int.random(in: 1...12)
+        @State var inputGiven = ""
 
+        // Has an answer been checked?
+        @State var answerChecked = false
+
+        // Was the answer given actually correct?
+        @State var answerCorrect = false
+
+        // MARK: Computed properties
+        // What is the correct product?
+        var correctProduct: Int {
+            return additionand + additioner
+        }
+        
+        var body: some View {
+                    
+            VStack(spacing: 0) {
+                HStack {
+                    Text("+")
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .trailing) {
+                        Text("\(additionand)")
+                        Text("\(additioner)")
+                    }
+                }
+                
+                Divider()
+                
+                HStack {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
+                        //        CONDITION      true  false
+                        .opacity(answerCorrect ? 1.0 : 0.0)
+                    Spacer()
+                    TextField("",
+                              text: $inputGiven)
+                        .multilineTextAlignment(.trailing)
+                }
+
+                Button(action: {
+
+                    // Answer has been checked!
+                    answerChecked = true
+
+                    // Convert the input given to an integer, if possible
+                    guard let productGiven = Int(inputGiven) else {
+                        // Sadness, not a number
+                        answerCorrect = false
+                        return
+                    }
+
+                    // Check the answer!
+                    if productGiven == correctProduct {
+                        // Celebrate! 👍🏼
+                        answerCorrect = true
+                    } else {
+                        // Sadness, they gave a number, but it's correct 😭
+                        answerCorrect = false
+                    }
+                }, label: {
+                    Text("Check Answer")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+            .font(.system(size: 72))
+            
+        }
+    }
 struct AdditionView_Previews: PreviewProvider {
     static var previews: some View {
         AdditionView()
--- Arithmetic Ace/ClassExampleView.swift
+++ Arithmetic Ace/ClassExampleView.swift
@@ -8,74 +8,84 @@
 import SwiftUI
 
 struct ClassExampleView: View {
-    // MARK: Stored Properties
-    let multiplicand = Int.random(in: 1...12)
-    let multiplier = Int.random(in: 1...12)
-    @State var inputGiven = ""
-    
-    
-    // MARK: Computed Property
-    // What is the correct product?
-    var correctProduct: Int {
-        return multiplicand * multiplier
-    }
-    
-    var body: some View {
-        
-        VStack(spacing: 0) {
-            HStack {
-                Text("✕")
-                
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("\(multiplicand)")
-                    Text("\(multiplier)")
-                }
-            }
-           
-            
-            
-            Divider()
-            
-            HStack {
-                
-                Image(systemName: "checkmark.circle")
-                    .foregroundColor(.green)
-                Spacer()
-                TextField("",
-                          text: $inputGiven)
-                    .multilineTextAlignment(.trailing)
-            }
-            Button(action: {
-                // Convert the input given to an integer, if possible
-                guard let productGiven = Int(inputGiven) else {
-                    // Sadness, not a number
-                    return
-                }
-                // Check the answer!
-                if productGiven == correctProduct {
-                    // Celebrate!
-                } else {
-                    // Sadness, they gave a number, but it's not correct. 
-                }
-                
-            }, label: {
-                Text("Check Answer")
-                    
-                    .font(.largeTitle)
-            })
-                .padding()
-                .buttonStyle(.bordered)
-            
-            
-            
-            Spacer()
+    // MARK: Stored properties
+        let multiplicand = Int.random(in: 1...12)
+        let multiplier = Int.random(in: 1...12)
+        @State var inputGiven = ""
+
+        // Has an answer been checked?
+        @State var answerChecked = false
+
+        // Was the answer given actually correct?
+        @State var answerCorrect = false
+
+        // MARK: Computed properties
+        // What is the correct product?
+        var correctProduct: Int {
+            return multiplicand * multiplier
         }
-        .padding(.horizontal)
-        .font(.system(size: 72))
         
+        var body: some View {
+                    
+            VStack(spacing: 0) {
+                HStack {
+                    Text("✕")
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .trailing) {
+                        Text("\(multiplicand)")
+                        Text("\(multiplier)")
+                    }
+                }
+                
+                Divider()
+                
+                HStack {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
+                        //        CONDITION      true  false
+                        .opacity(answerCorrect ? 1.0 : 0.0)
+                    Spacer()
+                    TextField("",
+                              text: $inputGiven)
+                        .multilineTextAlignment(.trailing)
+                }
+
+                Button(action: {
+
+                    // Answer has been checked!
+                    answerChecked = true
+
+                    // Convert the input given to an integer, if possible
+                    guard let productGiven = Int(inputGiven) else {
+                        // Sadness, not a number
+                        answerCorrect = false
+                        return
+                    }
+
+                    // Check the answer!
+                    if productGiven == correctProduct {
+                        // Celebrate! 👍🏼
+                        answerCorrect = true
+                    } else {
+                        // Sadness, they gave a number, but it's not correct 😭
+                        answerCorrect = false
+                    }
+                }, label: {
+                    Text("Check Answer")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+            .font(.system(size: 72))
+            
+        }
     }
-}
 
 struct ClassExampleView_Previews: PreviewProvider {
     static var previews: some View {
